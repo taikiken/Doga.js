6b78fa96c3949676265f6272d4b4e8373d3d7f00
taikiken <denkosekka@gmail.com>
2015-10-08 17:35:24 +0900

    1st

---
+++ docs/api.js
@@ -0,0 +1,20 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Cue",
+        "EventDispatcher",
+        "Player"
+    ],
+    "modules": [
+        "Doga"
+    ],
+    "allModules": [
+        {
+            "displayName": "Doga",
+            "name": "Doga",
+            "description": "## 動画(HTML 5 Video)コントロール\n依存ファイルはありません。\n### Support\nIE 9, IE 10, IE 11, Edge latest, Chrome latest, Safari latest, Firefox latest"
+        }
+    ],
+    "elements": []
+} };
+});
